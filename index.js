27ebf20080251aad4a810530cabd378b74a2b04b
Christoph Honal <christoph.honal@web.de>
2017-02-02 18:30:45 +0100

    Comments

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ var fs = require('fs');
 var excelbuilder = require('msexcel-builder');
 
 var workbook = excelbuilder.createWorkbook('./result', 'arrival_imdb_comments.xlsx')
-var sheet1 = workbook.createSheet('Arrival', 5, 770);
+var sheet1 = workbook.createSheet('Arrival', 6, 770);
 
-var header = ["date", "ratingPercent", "usefulPercent", "title", "text"];
+var header = ["date", "ratingPercent", "usefulPercent", "from", "title", "text"];
 for(var k = 0; k < header.length; k++) sheet1.set(k + 1, 1, header[k]);
 
 var rowNumber = 0;
@@ -33,6 +33,12 @@ for (var i = 0; i < 77; i++)
             var ratingParts = meta.children("img").first().attr("alt").split("/");
             ratingPercent = (parseInt(ratingParts[0]) / parseInt(ratingParts[1])) * 100;
         }
+        var fromParts = meta.children("small").eq(1).text().split(" ");
+        var from = "";
+        if(fromParts[0] == "from") {
+            fromParts.shift();
+            from = fromParts.join(" ");
+        }
         var dateText = meta.children("small").last().text();
         var dateParts = dateText.split(" ");
         var day = parseInt(dateParts[0]);
@@ -40,7 +46,7 @@ for (var i = 0; i < 77; i++)
         var month = (new Date(Date.parse(dateParts[1] +" 1, 2012"))).getMonth() + 1;
         var cleanr = /[\u0000-\u0008\u000B-\u000C\u000E-\u001F\uD800-\uDFFF\uFFFE-\uFFFF]/gi;
         var entry = [day + "." + month + "." + year, ratingPercent.toString().replace(".", ","), 
-            usefulPercent.toString().replace(".", ","), title.replace(cleanr, ""), text.replace(cleanr, "")];
+            usefulPercent.toString().replace(".", ","), from, title.replace(cleanr, ""), text.replace(cleanr, "")];
         for(var k = 0; k < entry.length; k++) sheet1.set(k + 1, rowNumber + 2, entry[k]);
         console.log("Extraced comment #" + j + " from file #" + i + " (comment #" + rowNumber + ")");
         rowNumber++;
